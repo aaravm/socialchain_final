7c0c57e40070a2885c4a6ebb7b9b53318d4ae09d
Jineshbansal <732005jinesh@gmail.com>
2024-02-11 22:50:18 +0530

    likes and comments connected with wallet

--- src/components/FeedPost.tsx
+++ src/components/FeedPost.tsx
@@ -4,16 +4,228 @@ import React, {useState} from "react";
 import { ExplorePublicationsQuery } from "../graphql/generated";
 import styles from "../styles/FeedPost.module.css";
 import { BiCommentDetail, BiLike } from "react-icons/bi";
+import  {ethers} from "ethers";
+import fetch from 'node-fetch';
+
+
+async function main() {
+
+
+  const contractaddress = "0x0e4b2c277d559128310e77d688b6a790203c0e89";
+  const abi=
+    [
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_id",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_content",
+            "type": "string"
+          }
+        ],
+        "name": "addComment",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_id",
+            "type": "string"
+          }
+        ],
+        "name": "addLikes",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "sender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "string",
+            "name": "content",
+            "type": "string"
+          }
+        ],
+        "name": "MessageSent",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "sender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint32",
+            "name": "num",
+            "type": "uint32"
+          }
+        ],
+        "name": "like",
+        "type": "event"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_id",
+            "type": "string"
+          }
+        ],
+        "name": "subLikes",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "comments",
+        "outputs": [
+          {
+            "internalType": "address",
+            "name": "sender",
+            "type": "address"
+          },
+          {
+            "internalType": "string",
+            "name": "content",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_id",
+            "type": "string"
+          }
+        ],
+        "name": "getComments",
+        "outputs": [
+          {
+            "components": [
+              {
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+              },
+              {
+                "internalType": "string",
+                "name": "content",
+                "type": "string"
+              }
+            ],
+            "internalType": "struct Social.Comment[]",
+            "name": "",
+            "type": "tuple[]"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "name": "likes",
+        "outputs": [
+          {
+            "internalType": "uint32",
+            "name": "",
+            "type": "uint32"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_id",
+            "type": "string"
+          }
+        ],
+        "name": "showLikes",
+        "outputs": [
+          {
+            "internalType": "uint32",
+            "name": "",
+            "type": "uint32"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ];
+  if(typeof window.ethereum!=="undefined")
+  {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    console.log("provider",provider);
+    const contract=new ethers.Contract(
+      contractaddress,
+      abi,
+      provider
+    );
+    console.log("contract",contract);
+
+    try{
+      const data=await contract.showLikes("12");
+      console.log("data123",data);
+    }catch(err){
+      console.log("Error:",err);
+
+    }
+  }
+
+  
+}
 
 type Props = {
   publication: ExplorePublicationsQuery["explorePublications"]["items"][0];
 };
-
-
+const [openComment, setOpenComment] = useState(false);
+const [comment, setComment] = useState('');
+main();
 
 export default function FeedPost({ publication }: Props) {
-  const [openComment, setOpenComment] = useState(false);
-  const [comment, setComment] = useState('');
   console.log(publication);
 
   const handleComment = () => {
